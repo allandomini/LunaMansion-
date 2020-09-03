e70b2cea2d6b3a3cfdd4c3840a7408602963dd26
allandomini <62321326+allandomini@users.noreply.github.com>
2020-09-03 16:16:02 -0300

    Add files via upload

---
+++ MinhasCoisinhas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinhasCoisinhas : MonoBehaviour
+{
+    
+    public Animator Animator;
+
+     public LayerMask Tree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+             
+        
+     
+    }
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           
+             Animator.SetBool("ground", false);
+               Animator.SetBool("treefino",true);
+            
+        }
+    }
+    // Update is called once per frame
+}
